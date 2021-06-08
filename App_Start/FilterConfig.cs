1075da3aa4f9e9b63cf8b8624551df5e4454dc75
mishrab <bibhup_mishra@yahoo.com>
2021-06-08 06:51:05 -0500

    Application Insight Integration

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace aspnet_mvc_devops_demo
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
